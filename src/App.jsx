eca9048915efcb755366f9015c95d596c1139122
kevin <kevin.rojokevin17@gmail.com>
2024-08-28 20:19:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+export function App() {
+  return (
+    <div>
+      <form className="formulario-1">
+        <h1 className="title">Registrate</h1>
+        <label htmlFor="nombre" className="text">
+          Nombre
+        </label>
+      </form>
+    </div>
+  );
+}
