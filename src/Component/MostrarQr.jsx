72b194e4751fd3b926be9291b4603c9493eac300
kevin <kevin.rojokevin17@gmail.com>
2024-09-16 17:47:39 -0300

    copiado

--- src/Component/MostrarQr.jsx
+++ src/Component/MostrarQr.jsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
 
 export const MostrarQr = ({ textButton, mostrarButton }) => {
-
-const [mostrarButton, setMostrarButton] = useState(true)
-  
-  const buttonClassName = mostrarButton ? "btn1" : "ocultar";
-
-  return <input type="submit" value={textButton} className={buttonClassName} />;
+  return <input type="submit" value={textButton} className="btn1" />;
 };
